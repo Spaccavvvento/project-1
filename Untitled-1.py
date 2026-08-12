4690d7148a22384d1f9677f34dfb00aa3ed2fa33
Elliott Spaccavento <elliottsp@protonmail.com>
2026-08-12 17:13:30 +1000

    arithemteic

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,52 @@
 print ("Hello, World!")
 
+# Comment wow shocking 
+#only use f stringn if need to add text to an output
+
+#variables = reusbale for a value
+
+
+
+#strings = any value (i think)
+city = "canberra"
+print (f"location {city}") #F string and then everything needs to be in the same quotations for it to work
+
+#integers = whole numbers
+population = 500000
+print (f"population {population}") 
+
+#float = numbers with decimal points
+temperature = 25.5
+print (f"temperature {temperature}")
+
+#boolean = either true or false
+is_in_Australia = True #needs to be capitalised 
+print (f"is in Australia {is_in_Australia}") #typically dont output diretcly 
+
+
+
+
+#if statment will come back to this later
+
+if is_in_Australia:
+    print ("This city is in Australia") #needs to be indented to be part of the if statement
+else: #needs to be non indepnted 
+    print ("This city is not in Australia") #needs to be indented to be part of the else statement
+
+
+
+#math and how to do it in python
+dog = 1
+dog += 1 #this is the same as dog = dog + 1 ==== called an arguemneted assignemenrt operator
+print (dog)
+
+#same follows for subtraction, multiplication and division
+# / = float divison
+#//  = returns an imterger 
+#% = returns the remainder of a division put number divided by after the remiander
+
+
+
+#type castung = changing the type of a variable to another type
+
+
